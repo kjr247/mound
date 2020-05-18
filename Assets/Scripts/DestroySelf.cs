6cb2cc5d1e44552037aedc618b55723e36c13e06
Kyle Rebstock <kjr247@gmail.com>
2020-05-17 20:51:38 -0400

    added mouse trail scripts, and tweaked scene, prefabs, gravity, and platform

---
+++ Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) {
+        
+        if(other.gameObject.tag == "Platform") {
+            Destroy (gameObject, 0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject droppedThing;
+    public float spawnRate = 2.0f;
+    public float nextTime  = 0;
+    private int maxDrops = 30;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        var rand = Random.Range( 0f, 1f);
+        var randx = Random.Range( -10.0f, 10.0f);
+
+        if (Time.time >= nextTime && maxDrops >= 0)
+        {
+            maxDrops--;
+            Instantiate(droppedThing, new Vector3(randx, 10, 0), Quaternion.identity);
+            nextTime += (spawnRate * rand);
+        }
+    }
+}
